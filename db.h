ee032670437494c94f0f1907f2d08aa5aa057de8
Martin Linkhorst <martin.linkhorst+github.com@posteo.de>
2015-11-28 20:08:51 +0100

    increase requirements for "good" nodes

--- db.h
+++ db.h
@@ -12,11 +12,11 @@
 
 #define MIN_RETRY 1000
 
-#define REQUIRE_VERSION 60001
+#define REQUIRE_VERSION 70001
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 0 : 150000;
+    return testnet ? 30000 : 800000;
 }
 
 std::string static inline ToString(const CService &ip) {
