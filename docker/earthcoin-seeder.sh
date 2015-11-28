dd8fb61008812a06815d73bcfef5c7ab33310a31
Martin Linkhorst <martin.linkhorst+github.com@posteo.de>
2015-11-28 22:21:39 +0100

    soa record is required now

--- docker/earthcoin-seeder.sh
+++ docker/earthcoin-seeder.sh
@@ -2,4 +2,4 @@
 
 # see: https://github.com/earthcoinproject/earthcoin-seeder/blob/master/README
 exec /sbin/setuser root \
-  /usr/src/earthcoin-seeder/dnsseed -h dnsseed.earthcointools.org -n seed.earthcointools.org >>/var/log/earthcoind-seeder.log 2>&1
+  /usr/src/earthcoin-seeder/dnsseed -h dnsseed.earthcointools.org -n seed.earthcointools.org -m dnsadmin.earthcointools.org >>/var/log/earthcoind-seeder.log 2>&1
