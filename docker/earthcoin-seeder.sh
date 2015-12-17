c0a40a7d1ac9e2b81c1fdbdbe54fb38c89d1dae0
Martin Linkhorst <martin.linkhorst+github.com@posteo.de>
2015-12-17 15:43:52 +0100

    do not use passenger baseimage & don't run as root

--- docker/earthcoin-seeder.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# see: https://github.com/earthcoinproject/earthcoin-seeder/blob/master/README
-exec /sbin/setuser root \
-  /usr/src/earthcoin-seeder/dnsseed -h dnsseed.earthcointools.org -n seed.earthcointools.org -m dnsadmin.earthcointools.org >>/var/log/earthcoind-seeder.log 2>&1
